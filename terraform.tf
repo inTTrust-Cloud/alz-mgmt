6d8e32d4774b8f115637e6bd4c350a39a4ca1bfb
Diimitrios Syropoulos <dsyropoulos@inttrust.gr>
2025-10-08 14:55:13 +0300

    Add amba to the landing zone

--- terraform.tf
+++ terraform.tf
@@ -25,6 +25,10 @@ provider "alz" {
   library_overwrite_enabled = true
   library_references = [
     {
+      path = "platform/amba"
+      ref  = "2025.09.2" # check the latest library version https://github.com/Azure/Azure-Landing-Zones-Library/tags
+    },
+    {
       custom_url = "${path.root}/lib"
     }
   ]
